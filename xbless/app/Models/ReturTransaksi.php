e9a120528e1f9532f81d142d50edd7b7c2579003
adincomara <adincomara@gmail.com>
2022-02-14 16:45:32 +0700

    list retur

--- xbless/app/Models/ReturTransaksi.php
+++ xbless/app/Models/ReturTransaksi.php
@@ -9,4 +9,18 @@ class ReturTransaksi extends Model
 {
     use HasFactory;
     protected $table = 'retur_transaksi';
+
+    public function penjualan(){
+
+    }
+
+    public function pembelian(){
+
+    }
+    public function getsales(){
+        return $this->hasOne(Sales::class, 'id', 'id_sales');
+    }
+    public function gettoko(){
+        return $this->hasOne(Toko::class, 'id', 'id_toko');
+    }
 }
