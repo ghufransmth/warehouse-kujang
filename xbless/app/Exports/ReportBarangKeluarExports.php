7aaa045d19d090189024d9528a3a8c0deefc70d5
adincomara <adincomara@gmail.com>
2022-02-15 08:21:38 +0700

    update report

--- xbless/app/Exports/ReportBarangKeluarExports.php
+++ xbless/app/Exports/ReportBarangKeluarExports.php
@@ -24,72 +24,26 @@ use App\Models\ReportStock;
 class ReportBarangKeluarExports implements FromView, ShouldAutoSize
 {
 
-    protected $filter_produk;
-    protected $filter_perusahaan;
-    protected $filter_gudang;
-    protected $filter_tgl_start;
-    protected $filter_tgl_end;
-    protected $masuk;
-
-    public function __construct($filter_produk, $filter_perusahaan, $filter_gudang, $filter_tgl_start, $filter_tgl_end, $masuk)
+    protected $view;
+    protected $data;
+    protected $detail;
+    public function __construct($view, $data, $detail_penjualan)
     {
-        $this->filter_produk        = $filter_produk;
-        $this->filter_perusahaan    = $filter_perusahaan;
-        $this->filter_gudang        = $filter_gudang;
-        $this->filter_tgl_start     = $filter_tgl_start;
-        $this->filter_tgl_end       = $filter_tgl_end;
-        $this->masuk                = $masuk;
+        $this->view        = $view;
+        $this->data        = $data;
+        $this->detail      = $detail_penjualan;
     }
 
     public function view(): View
     {
 
+        $data = $this->data;
+        $view = $this->view;
+        $detail_penjualan = $this->detail;
 
-        $perusahaan = Perusahaan::find($this->filter_perusahaan);
-
-        $querydb =  ReportStock::whereHas('transaction_order_bm_bl', function ($q) {
-            $q->where('flag_status', 0);
-        })->with(['getperusahaan', 'getinvoice', 'produk_beli', 'getproduct' => function ($query) {
-            $query->with('getsatuan');
-        }, 'getgudang', 'transaction_detail', 'transaction_order_bm_bl' => function ($query) {
-            $query->with('getmember');
-        }]);
-
-        $querydb->where('note', 'like', '%' . $this->masuk . '%');
-
-        if ($this->filter_produk != "") {
-            $querydb->where('product_id', $this->filter_produk);
-        }
-
-        if ($this->filter_tgl_start != "" && $this->filter_tgl_end != "") {
-            $querydb->whereDate('updated_at', '>=', date('Y-m-d', strtotime($this->filter_tgl_start)));
-            $querydb->whereDate('updated_at', '<=', date('Y-m-d', strtotime($this->filter_tgl_end)));
-        }
-        if ($this->filter_perusahaan != "") {
-            $querydb->where('perusahaan_id', $this->filter_perusahaan);
-        }
-        if ($this->filter_gudang != "") {
-            $querydb->whereIn('gudang_id', $this->filter_gudang);
-        }
-
-
-
-        $data = $querydb->get();
-
-
-        foreach ($data as $key => $value) {
-            $value->conv_tgl          = date('Y-m-d', strtotime($value->updated_at));
-        }
-
-        $data = $data->sortByDesc('conv_tgl')->values();
-        return view('backend/report/barang_keluar/index_barangkeluar_excel', [
-            'data' => $data,
-            'filter_produk' => $this->filter_produk,
-            'filter_perusahaan'   => $this->filter_perusahaan,
-            'filter_gudang' => $this->filter_gudang,
-            'perusahaan' => $perusahaan,
-            'filter_tgl_start' => $this->filter_tgl_start,
-            'filter_tgl_end'  => $this->filter_tgl_end
+        return view($view, [
+            'penjualan' => $data,
+            'detail_penjualan' => $detail_penjualan
         ]);
     }
 }
